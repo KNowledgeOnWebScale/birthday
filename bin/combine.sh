67bdfc768c84f5243034cdc3bc5340d576cd06b2
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2024-10-19 13:42:34 +0200

    Adding the -s option to show generated surfaces

--- bin/combine.sh
+++ bin/combine.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-eye --nope --no-bnode-relabeling --quiet n3s/compiler.n3s n3s/equivalence.n3s n3s/query.n3s "$@"
+QTYPE=n3s/query.n3s
+
+while getopts ":s" option; do
+   case $option in
+      s) QTYPE=n3s/show.n3s
+        ;;
+   esac
+done
+
+shift $((OPTIND-1))
+
+eye --nope --no-bnode-relabeling --quiet n3s/compiler.n3s n3s/equivalence.n3s $QTYPE "$@"
